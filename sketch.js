26a97f05013989896a99fad79a548e9d9035872d
sararo <ro.hsara@gmail.com>
2021-09-30 20:00:57 -0400

    solfege hand sign scale player functionality

--- sketch.js
+++ sketch.js
@@ -1,44 +1,270 @@
-const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvase')[0];
-const canvasCtx = canvasElement.getContext('2d');
+let model, video, keypoints, predictions=[]; 
+let fft, osc;
+// Create a KNN classifier
+const classifier = knnClassifier.create();
+
+const LABELS_MAP = {
+	'Do': [262, 'C4'],
+	'Re': [294, 'D4'],
+	'Mi': [330, 'E4'],
+	'Fa': [350, 'F4'],
+	'Sol': [392, 'G4'], 
+	'La': [440, 'A4'],
+	'Ti': [494, 'B4'],
+	'Do5': [523, 'C5']
+};
+
+function preload() {
+	video = createCapture(VIDEO, () => {
+	  loadHandTrackingModel();
+	});
+	video.hide();
+	// Create the UI buttons
+	createButtons();
+}
 
 function setup() {
-	createCanvas(640, 480);
+	const canvas = createCanvas(600, 400);
+	canvas.parent('canvasContainer');
+	
+	osc = new p5.Oscillator('sine');
+	osc.amp(0.1);
+	// osc.start();
 
-	const camera = new Camera(videoElement, {
-		onFrame: async () => {
-			await hands.send({image: videoElement});
-		},
-		width: 640,
-		height: 480
-	});
-	camera.start();
+	fft = new p5.FFT();
+}
 
-	const hands = new Hands({locateFile: (file) => {
-		return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-	}});
-	hands.setOptions({
-		maxNumHands: 2,
-		minDetectionConfidence: 0.5,
-		minTrackingConfidence: 0.5
-	});
-	hands.onResults(onResults);
+async function loadHandTrackingModel() {
+	// Load the MediaPipe handpose model.
+	model = await handpose.load();
+	select('#status').html('Hand Tracking Model Loaded')
+	predictHand();
 }
 
 function draw() {
-	background(220);
-}
-
-function onResults(results){
-	canvasCtx.save();
-	canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-	canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-
-	if(results.multiHandLandmarks) {
-		for(const landmarks of results.multiHandLandmarks) {
-			drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {color: '#00FF00', lineWidth: 5});
-			drawLandMarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
-		}
+	background(255);
+	if (model) image(video, 0, 0);
+	if (predictions.length > 0) {
+	  // We can call both functions to draw all keypoints and the skeletons
+	  drawKeypoints();
+	  drawSkeleton();
 	}
-	canvasCtx.restore();
+	
 }
+
+async function predictHand() {
+	// Pass in a video stream (or an image, canvas, or 3D tensor) to obtain a
+	// hand prediction from the MediaPipe graph.
+	predictions = await model.estimateHands(video.elt);
+  
+	setTimeout(() => predictHand(), 200);
+}
+
+// Add the current hand tracking data to the classifier
+function addExample(label) {
+	if (predictions.length > 0) {
+	  const features = predictions[0].landmarks;
+	  const tensors = tf.tensor(features)
+	  // Add an example with a label to the classifier
+	  classifier.addExample(tensors, label);
+	  updateCounts();
+	} else {
+	  console.log('No gesture is detected')
+	}
+}
+
+// Predict the current frame.
+async function classify() {
+	// Get the total number of labels from classifier
+	const numLabels = classifier.getNumClasses();
+	if (numLabels <= 0) {
+	  console.error('There is no examples in any label');
+	  osc.stop();
+	  return;
+	}
+	if (predictions.length > 0) {
+	    osc.start();
+		const results = await classifier.predictClass(tf.tensor(predictions[0].landmarks));
+		if (results.confidences) {
+			const confidences = results.confidences;
+			// Change the freq of osc
+			const freq = LABELS_MAP[results.label][0];
+			osc.freq(freq);
+			// result.label is the label that has the highest confidence
+			if (results.label) {
+				select('#result').html(results.label);
+				select('#confidence').html(`${confidences[results.label] * 100} %`);
+			}
+
+			select('#confidenceDo').html(`${confidences['Do'] ? confidences['Do'] * 100 : 0} %`);
+			select('#confidenceRe').html(`${confidences['Re'] ? confidences['Re'] * 100 : 0} %`);
+			select('#confidenceMi').html(`${confidences['Mi'] ? confidences['Mi'] * 100 : 0} %`);
+			select('#confidenceFa').html(`${confidences['Fa'] ? confidences['Fa'] * 100 : 0} %`);
+			select('#confidenceSol').html(`${confidences['Sol'] ? confidences['Sol'] * 100 : 0} %`);
+			select('#confidenceLa').html(`${confidences['La'] ? confidences['La'] * 100 : 0} %`);
+			select('#confidenceTi').html(`${confidences['Ti'] ? confidences['Ti'] * 100 : 0} %`);
+			select('#confidenceDo5').html(`${confidences['Do5'] ? confidences['Do5'] * 100 : 0} %`);
+		}
+		classify();
+  	} else {
+    	setTimeout(() => classify(), 1000);
+  	}
+}
+
+// Update the example count for each label	
+function updateCounts() {
+	const counts = classifier.getClassExampleCount();
+
+	select('#exampleDo').html(counts['Do'] || 0);
+	select('#exampleRe').html(counts['Re'] || 0);
+	select('#exampleMi').html(counts['Mi'] || 0);
+	select('#exampleFa').html(counts['Fa'] || 0);
+	select('#exampleSol').html(counts['Sol'] || 0);
+	select('#exampleLa').html(counts['La'] || 0);
+	select('#exampleTi').html(counts['Ti'] || 0);
+	select('#exampleDo5').html(counts['Do5'] || 0);
+}
+
+// Clear the examples in one label
+function clearLabel(label) {
+	classifier.clearClass(label);
+	updateCounts();
+}
+  
+// Clear all the examples in all labels
+function clearAllLabels() {
+	classifier.clearAllClasses();
+	updateCounts();
+}
+  
+// A util function to create UI buttons
+function createButtons() {
+	// When the A button is pressed, add the current frame
+	// from the video with a label of "do" to the classifier
+	buttonC = select('#addClassDo');
+	buttonC.mousePressed(function(){
+		addExample('Do');
+	}); 
+	
+	buttonD = select('#addClassRe');
+	buttonD.mousePressed(function(){
+		addExample('Re');
+	}); 
+	
+	buttonE = select('#addClassMi');
+	buttonE.mousePressed(function(){
+		addExample('Mi');
+	}); 
+	
+	buttonF = select('#addClassFa');
+	buttonF.mousePressed(function(){
+		addExample('Fa');
+	}); 
+	
+	buttonG = select('#addClassSol');
+	buttonG.mousePressed(function(){
+		addExample('Sol');
+	}); 
+	
+	buttonA = select('#addClassLa');
+	buttonA.mousePressed(function(){
+		addExample('La');
+	}); 
+
+	buttonB = select('#addClassTi');
+	buttonB.mousePressed(function(){
+		addExample('Ti');
+	}); 
+
+	buttonC5 = select('#addClassDo5');
+	buttonC5.mousePressed(function(){
+		addExample('Do5');
+	}); 
+	
+	// Reset Buttons
+	resetBtnC = select('#resetDo');
+	resetBtnC.mousePressed(function(){
+		addExample('Do');
+	}); 
+	
+	resetBtnD = select('#resetRe');
+	resetBtnD.mousePressed(function(){
+		addExample('Re');
+	}); 
+	
+	resetBtnE = select('#resetMi');
+	resetBtnE.mousePressed(function(){
+		addExample('Mi');
+	}); 
+	
+	resetBtnF = select('#resetFa');
+	resetBtnF.mousePressed(function(){
+		addExample('Fa');
+	}); 
+	
+	resetBtnG = select('#resetSol');
+	resetBtnG.mousePressed(function(){
+		addExample('Sol');
+	}); 
+	
+	resetBtnA = select('#resetLa');
+	resetBtnA.mousePressed(function(){
+		addExample('La');
+	}); 
+
+	resetBtnB = select('#resetTi');
+	resetBtnB.mousePressed(function(){
+		addExample('Ti');
+	}); 
+
+	resetBtnC5 = select('#resetDo5');
+	resetBtnC5.mousePressed(function(){
+		addExample('Do5');
+	}); 
+
+	// Predict Button
+	buttonPredict = select('#buttonPredict');
+	buttonPredict.mousePressed(classify);
+	
+	// Clear all classes button
+	buttonClearAll = select('#clearAll');
+	buttonClearAll.mousePressed(clearAllLabels);
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+	let prediction = predictions[0];
+	for (let j = 0; j < prediction.landmarks.length; j++) {
+	  let keypoint = prediction.landmarks[j];
+	  fill(255, 0, 0);
+	  noStroke();
+	  ellipse(keypoint[0], keypoint[1], 10, 10);
+	}
+}
+  
+  // A function to draw the skeletons
+function drawSkeleton() {
+	let annotations = predictions[0].annotations;
+	stroke(255, 0, 0);
+	for (let j = 0; j < annotations.thumb.length - 1; j++) {
+	  line(annotations.thumb[j][0], annotations.thumb[j][1], annotations.thumb[j + 1][0], annotations.thumb[j + 1][1]);
+	}
+	for (let j = 0; j < annotations.indexFinger.length - 1; j++) {
+	  line(annotations.indexFinger[j][0], annotations.indexFinger[j][1], annotations.indexFinger[j + 1][0], annotations.indexFinger[j + 1][1]);
+	}
+	for (let j = 0; j < annotations.middleFinger.length - 1; j++) {
+	  line(annotations.middleFinger[j][0], annotations.middleFinger[j][1], annotations.middleFinger[j + 1][0], annotations.middleFinger[j + 1][1]);
+	}
+	for (let j = 0; j < annotations.ringFinger.length - 1; j++) {
+	  line(annotations.ringFinger[j][0], annotations.ringFinger[j][1], annotations.ringFinger[j + 1][0], annotations.ringFinger[j + 1][1]);
+	}
+	for (let j = 0; j < annotations.pinky.length - 1; j++) {
+	  line(annotations.pinky[j][0], annotations.pinky[j][1], annotations.pinky[j + 1][0], annotations.pinky[j + 1][1]);
+	}
+  
+	line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.thumb[0][0], annotations.thumb[0][1]);
+	line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.indexFinger[0][0], annotations.indexFinger[0][1]);
+	line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.middleFinger[0][0], annotations.middleFinger[0][1]);
+	line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.ringFinger[0][0], annotations.ringFinger[0][1]);
+	line(annotations.palmBase[0][0], annotations.palmBase[0][1], annotations.pinky[0][0], annotations.pinky[0][1]);
+}  
